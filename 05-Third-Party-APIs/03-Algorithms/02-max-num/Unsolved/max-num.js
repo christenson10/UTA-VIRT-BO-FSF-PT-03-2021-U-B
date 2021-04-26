a3e7b247c446a47694de594187ed12e59c104e52
Tori Mason <torimason@sbcglobal.net>
2021-04-26 18:18:30 -0500

    Week 7 Files

--- 05-Third-Party-APIs/03-Algorithms/02-max-num/Unsolved/max-num.js
+++ 05-Third-Party-APIs/03-Algorithms/02-max-num/Unsolved/max-num.js
@@ -1,3 +1,25 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+var maxNum = function(arr) {
+    //start number
+    let bigNum = 0;
+    //iterating over arr 
+    for (var i = 0; i < arr.length; i ++){
+        //secondary index for number next to i
+        let j = i + 1
+        //if j is less than our arr lenght run the if statement
+        if(j < arr.length){
+            //if num at postion i is greater than num at position j
+            if(arr[i] > arr[j]){
+                //update bignum to number at position i 
+                bigNum = arr[i]
+            }
+            else{
+                //update num to postion j
+                bigNum = arr[j];
+            }
+        }
+    }
+            //returning our final number (the biggest number)
+            return bigNum;
+};
