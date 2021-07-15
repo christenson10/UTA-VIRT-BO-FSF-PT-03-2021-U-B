c01e02ffe9c220abde9bbb0e3226db639e08571b
Tori Mason <torimason@sbcglobal.net>
2021-07-15 10:27:35 -0500

    Week 18 Day 2 Solutions

---
+++ 18-NoSQL/01-Activities/02-Subdocuments/Unsolved/models/Note.js
@@ -0,0 +1,14 @@
+const { Schema, model } = require('mongoose');
+
+const noteSchema = new Schema({
+  noteId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  noteText: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = noteSchema;
